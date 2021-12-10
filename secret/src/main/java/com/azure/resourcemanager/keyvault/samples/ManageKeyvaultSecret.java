378dd650f6ccc95ff8dd8842bd63a1bab88c622e
Bradley Bridges <brbridge@microsoft.com>
2021-12-10 05:00:41 -0800

    Update samples to use new track 2 hybrid resource management libraries
    vm, resourcegroup, and storage were updated from the Azure track 2
    samples
    
    secret was rewritten to be in the same format

---
+++ secret/src/main/java/com/azure/resourcemanager/keyvault/samples/ManageKeyvaultSecret.java
@@ -0,0 +1,207 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.azure.resourcemanager.keyvault.samples;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.HttpResponse;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.util.serializer.JacksonAdapter;
+import com.azure.core.util.serializer.SerializerEncoding;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.keyvault.models.SkuName;
+import com.azure.resourcemanager.keyvault.models.Vault;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Azure Key Vault sample for managing secrets -
+ * - Create a key vault
+ * - Set a secret
+ * - Get a secret
+ * - Delete a key vault.
+ */
+
+public final class ManageKeyvaultSecret {
+    /**
+     * Main function which runs the actual sample.
+     *
+     * @param azureResourceManager instance of the azure client
+     * @param location the Azure location
+     * @param tokenCredential credential for secret client
+     * @param objectId object ID of secret client credential
+     * @return true if sample runs successfully
+     */
+    public static boolean runSample(AzureResourceManager azureResourceManager, String location,
+                                    TokenCredential tokenCredential, String objectId) {
+        final String vaultName = Utils.randomResourceName(azureResourceManager, "kv", 8);
+        final String secretName = Utils.randomResourceName(azureResourceManager, "s", 8);
+        final String secretValue = Utils.password();
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgkvs", 16);
+        try {
+
+
+            //=============================================================
+            // Create a key vault.
+
+            System.out.println("Creating a key vault with name: " + vaultName);
+
+            Vault vault = azureResourceManager.vaults().define(vaultName)
+                    .withRegion(location)
+                    .withNewResourceGroup(rgName)
+                    .defineAccessPolicy()
+                            .forObjectId(objectId)
+                            .allowSecretAllPermissions()
+                            .attach()
+                    .withDeploymentEnabled()
+                    .withTemplateDeploymentEnabled()
+                    .withSku(SkuName.STANDARD)
+                    .create();
+
+            System.out.println("Created a key vault with name: " + vaultName);
+            Utils.print(vault);
+
+
+            //=============================================================
+            // Set a secret.
+
+            SecretClient secretClient = new SecretClientBuilder()
+                    .vaultUrl(vault.vaultUri())
+                    .credential(tokenCredential)
+                    .buildClient();
+
+            System.out.println("Setting a secret with name: " + secretName + ", value: " + secretValue);
+
+            secretClient.setSecret(secretName, secretValue);
+
+            System.out.println("Set the secret with name: " + secretName);
+
+
+            //=============================================================
+            // Get a secret.
+
+            System.out.println("Getting the secret with name: " + secretName);
+
+            secretClient.getSecret(secretName);
+
+            System.out.println("Got the secret with name: " + secretName + ", value: " + secretValue);
+
+
+            //=============================================================
+            // Delete a key vault.
+
+            System.out.println("Deleting key vault with name: " + vaultName);
+
+            azureResourceManager.vaults().deleteById(vault.id());
+
+            System.out.println("Deleting key vault with name: " + vaultName);
+            return true;
+        } finally {
+
+            try {
+                System.out.println("Deleting Resource Group: " + rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
+            } catch (NullPointerException npe) {
+                System.out.println("Did not create any resources in Azure. No clean up is necessary");
+            } catch (Exception g) {
+                g.printStackTrace();
+            }
+        }
+    }
+
+    private static AzureEnvironment getAzureEnvironmentFromArmEndpoint(String armEndpoint) {
+        // Create HTTP client and request
+        HttpClient httpClient = HttpClient.createDefault();
+
+        HttpRequest request = new HttpRequest(HttpMethod.GET,
+                String.format("%s/metadata/endpoints?api-version=2019-10-01", armEndpoint))
+                .setHeader("accept", "application/json");
+
+        // Execute the request and read the response
+        HttpResponse response = httpClient.send(request).block();
+        if (response.getStatusCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusCode());
+        }
+        String body = response.getBodyAsString().block();
+        try {
+            ArrayNode metadataArray = JacksonAdapter.createDefaultSerializerAdapter()
+                    .deserialize(body, ArrayNode.class, SerializerEncoding.JSON);
+
+            if (metadataArray == null || metadataArray.isEmpty()) {
+                throw new RuntimeException("Failed to find metadata : " + body);
+            }
+
+            JsonNode metadata = metadataArray.iterator().next();
+            AzureEnvironment azureEnvironment = new AzureEnvironment(new HashMap<String, String>() {
+                {
+                    put("managementEndpointUrl", metadata.at("/authentication/audiences/0").asText());
+                    put("resourceManagerEndpointUrl", armEndpoint);
+                    put("galleryEndpointUrl", metadata.at("/gallery").asText());
+                    put("activeDirectoryEndpointUrl", metadata.at("/authentication/loginEndpoint").asText());
+                    put("activeDirectoryResourceId", metadata.at("/authentication/audiences/0").asText());
+                    put("activeDirectoryGraphResourceId", metadata.at("/graph").asText());
+                    put("storageEndpointSuffix", "." + metadata.at("/suffixes/storage").asText());
+                    put("keyVaultDnsSuffix", "." + metadata.at("/suffixes/keyVaultDns").asText());
+                }
+            });
+            return azureEnvironment;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * Main entry point.
+     * @param args the parameters
+     */
+    public static void main(String[] args) {
+        try {
+
+            //=============================================================
+            // Authenticate
+
+            final String armEndpoint = System.getenv("ARM_ENDPOINT");
+            final String location = System.getenv("RESOURCE_LOCATION");
+            final String objectId = System.getenv("AZURE_OBJECT_ID");
+
+            // Register Azure Stack cloud environment
+            final AzureProfile profile = new AzureProfile(getAzureEnvironmentFromArmEndpoint(armEndpoint));
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                    .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                    .build();
+
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                    .configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credential, profile)
+                    .withDefaultSubscription();
+
+            // Print selected subscription
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
+
+            runSample(azureResourceManager, location, credential, objectId);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private ManageKeyvaultSecret() {
+    }
+}
---
+++ storage/src/main/java/com/azure/resourcemanager/storage/samples/ManageStorageAccount.java
@@ -0,0 +1,218 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.azure.resourcemanager.storage.samples;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.HttpResponse;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.util.serializer.JacksonAdapter;
+import com.azure.core.util.serializer.SerializerEncoding;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
+import com.azure.resourcemanager.storage.models.StorageAccount;
+import com.azure.resourcemanager.storage.models.StorageAccountKey;
+import com.azure.resourcemanager.storage.models.StorageAccountSkuType;
+import com.azure.resourcemanager.storage.models.StorageAccounts;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Azure Storage sample for managing storage accounts -
+ * - Create a storage account
+ * - Get | regenerate storage account access keys
+ * - Create another storage account
+ * - List storage accounts
+ * - Delete a storage account.
+ */
+
+public final class ManageStorageAccount {
+    /**
+     * Main function which runs the actual sample.
+     *
+     * @param azureResourceManager instance of the azure client
+     * @param location the Azure location
+     * @return true if sample runs successfully
+     */
+    public static boolean runSample(AzureResourceManager azureResourceManager, String location) {
+        final String storageAccountName = Utils.randomResourceName(azureResourceManager, "sa", 8);
+        final String storageAccountName2 = Utils.randomResourceName(azureResourceManager, "sa2", 8);
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgSTMS", 8);
+        try {
+
+            // ============================================================
+            // Create a storage account
+
+            System.out.println("Creating a Storage Account");
+
+            StorageAccount storageAccount = azureResourceManager.storageAccounts().define(storageAccountName)
+                    .withRegion(location)
+                    .withNewResourceGroup(rgName)
+                    .withGeneralPurposeAccountKind()
+                    .withSku(StorageAccountSkuType.STANDARD_LRS)
+                    .create();
+
+            System.out.println("Created a Storage Account:");
+            Utils.print(storageAccount);
+
+
+            // ============================================================
+            // Get | regenerate storage account access keys
+
+            System.out.println("Getting storage account access keys");
+
+            List<StorageAccountKey> storageAccountKeys = storageAccount.getKeys();
+
+            Utils.print(storageAccountKeys);
+
+            System.out.println("Regenerating first storage account access key");
+
+            storageAccountKeys = storageAccount.regenerateKey(storageAccountKeys.get(0).keyName());
+
+            Utils.print(storageAccountKeys);
+
+
+            // ============================================================
+            // Create another storage account
+
+            System.out.println("Creating a 2nd Storage Account");
+
+            StorageAccount storageAccount2 = azureResourceManager.storageAccounts().define(storageAccountName2)
+                    .withRegion(location)
+                    .withNewResourceGroup(rgName)
+                    .withGeneralPurposeAccountKind()
+                    .withSku(StorageAccountSkuType.STANDARD_LRS)
+                    .create();
+
+            System.out.println("Created a Storage Account:");
+            Utils.print(storageAccount2);
+
+            // ============================================================
+            // List storage accounts
+
+            System.out.println("Listing storage accounts");
+
+            StorageAccounts storageAccounts = azureResourceManager.storageAccounts();
+
+            PagedIterable<StorageAccount> accounts = storageAccounts.listByResourceGroup(rgName);
+            for (StorageAccount sa : accounts) {
+                System.out.println("Storage Account " + sa.name()
+                        + " created @ " + sa.creationTime());
+            }
+
+            // ============================================================
+            // Delete a storage account
+
+            System.out.println("Deleting a storage account - " + storageAccount.name()
+                    + " created @ " + storageAccount.creationTime());
+
+            azureResourceManager.storageAccounts().deleteById(storageAccount.id());
+
+            System.out.println("Deleted storage account");
+            return true;
+        } finally {
+            try {
+                System.out.println("Deleting Resource Group: " + rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
+                System.out.println("Deleted Resource Group: " + rgName);
+            } catch (Exception e) {
+                System.out.println("Did not create any resources in Azure. No clean up is necessary");
+            }
+        }
+    }
+
+    private static AzureEnvironment getAzureEnvironmentFromArmEndpoint(String armEndpoint) {
+        // Create HTTP client and request
+        HttpClient httpClient = HttpClient.createDefault();
+
+        HttpRequest request = new HttpRequest(HttpMethod.GET,
+                String.format("%s/metadata/endpoints?api-version=2019-10-01", armEndpoint))
+                .setHeader("accept", "application/json");
+
+        // Execute the request and read the response
+        HttpResponse response = httpClient.send(request).block();
+        if (response.getStatusCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusCode());
+        }
+        String body = response.getBodyAsString().block();
+        try {
+            ArrayNode metadataArray = JacksonAdapter.createDefaultSerializerAdapter()
+                    .deserialize(body, ArrayNode.class, SerializerEncoding.JSON);
+
+            if (metadataArray == null || metadataArray.isEmpty()) {
+                throw new RuntimeException("Failed to find metadata : " + body);
+            }
+
+            JsonNode metadata = metadataArray.iterator().next();
+            AzureEnvironment azureEnvironment = new AzureEnvironment(new HashMap<String, String>() {
+                {
+                    put("managementEndpointUrl", metadata.at("/authentication/audiences/0").asText());
+                    put("resourceManagerEndpointUrl", armEndpoint);
+                    put("galleryEndpointUrl", metadata.at("/gallery").asText());
+                    put("activeDirectoryEndpointUrl", metadata.at("/authentication/loginEndpoint").asText());
+                    put("activeDirectoryResourceId", metadata.at("/authentication/audiences/0").asText());
+                    put("activeDirectoryGraphResourceId", metadata.at("/graph").asText());
+                    put("storageEndpointSuffix", "." + metadata.at("/suffixes/storage").asText());
+                    put("keyVaultDnsSuffix", "." + metadata.at("/suffixes/keyVaultDns").asText());
+                }
+            });
+            return azureEnvironment;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * Main entry point.
+     * @param args the parameters
+     */
+    public static void main(String[] args) {
+        try {
+
+            //=============================================================
+            // Authenticate
+
+            final String armEndpoint = System.getenv("ARM_ENDPOINT");
+            final String location = System.getenv("RESOURCE_LOCATION");
+
+            // Register Azure Stack cloud environment
+            final AzureProfile profile = new AzureProfile(getAzureEnvironmentFromArmEndpoint(armEndpoint));
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                    .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                    .build();
+
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                    .configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credential, profile)
+                    .withDefaultSubscription();
+
+            // Print selected subscription
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
+
+            runSample(azureResourceManager, location);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private ManageStorageAccount() {
+
+    }
+}
